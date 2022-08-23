d7a95fb382932a409ce4332a7a3bfe39b7a8b39c
Aldas25 <aldas.lenksas@gmail.com>
2022-08-23 12:17:33 +0300

    Done all tasks

--- lib/screens/tabs/stops_map_tab.dart
+++ lib/screens/tabs/stops_map_tab.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:gtfs_db/gtfs_db.dart';
+import 'package:transit/constants.dart';
 import 'package:transit/database/database_service.dart';
 import 'package:transit/widgets/app_future_loader.dart';
+import 'package:transit/widgets/map/layers/stops_marker_layer.dart';
 import 'package:transit/widgets/map/transit_map.dart';
 
 class StopsMapTab extends StatelessWidget {
@@ -10,11 +13,27 @@ class StopsMapTab extends StatelessWidget {
   Widget build(BuildContext context) {
     final database = DatabaseService.get(context);
 
-    return AppFutureBuilder<TransitMap>(
+    return AppFutureBuilder<List<Stop>>(
       future: database.getAllStops(),
-      builder: (BuildContext context, stops) {
-        return TransitMap(stops);
+      builder: (context, stops) {
+        return _StopsMapBody(stops: stops);
       },
+    );
+  }
+}
+
+class _StopsMapBody extends StatelessWidget {
+  final List<Stop> stops;
+
+  const _StopsMapBody({
+    required this.stops,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TransitMap(
+      center: defaultLatLng,
+      stopsLayer: StopsMarkerLayer(stops: stops),
     );
   }
 }
--- lib/widgets/map/layers/vehicle_positions_markers_layer.dart
+++ lib/widgets/map/layers/vehicle_positions_markers_layer.dart
@@ -3,17 +3,35 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:gtfs_db/gtfs_db.dart';
 import 'package:gtfs_realtime_bindings/gtfs_realtime_bindings.dart' as rt;
 import 'package:latlong2/latlong.dart';
+import 'package:transit/models/db_extensions.dart';
+import 'package:transit/navigator_routes.dart';
+import 'package:transit/screens/trip/trip_screen.dart';
+import 'package:vector_math/vector_math.dart';
 
 class VehiclePositionMarkersLayer {
   final List<rt.VehiclePosition> vehiclePositions;
   final List<Trip> trips;
   final List<TransitRoute> routes;
 
+  late Map<String, Trip> tripsLookup;
+  late Map<String, TransitRoute> routesLookup;
+
   VehiclePositionMarkersLayer({
     required this.vehiclePositions,
     required this.trips,
     required this.routes,
-  });
+  }) {
+    tripsLookup = new Map();
+    routesLookup = new Map();
+
+    for (final trip in trips) {
+      tripsLookup[trip.trip_id] = trip;
+    }
+
+    for (final route in routes) {
+      routesLookup[route.route_id] = route;
+    }
+  }
 
   MarkerLayerOptions buildLayer() {
     return MarkerLayerOptions(
@@ -25,17 +43,125 @@ class VehiclePositionMarkersLayer {
               vehiclePosition.position.longitude,
             ),
             anchorPos: AnchorPos.align(AnchorAlign.center),
-            width: 20,
-            height: 20,
+            //width: 20,
+            //height: 20,
             builder: (context) {
-              // TODO: Exercise 7
-              return Icon(
-                Icons.directions_bus,
-                size: 15,
-              );
+              return getVehicleContainer(vehiclePosition, context);
             },
           ),
       ],
     );
+  }
+
+  Widget getVehicleContainer(
+      rt.VehiclePosition vehiclePosition, BuildContext context) {
+    final tripId = vehiclePosition.trip.tripId;
+
+    final emptyContainer = getEmptyContainer(vehiclePosition);
+
+    if (!vehiclePosition.trip.hasTripId()) {
+      return emptyContainer;
+    }
+
+    final trip = tripsLookup[tripId];
+
+    if (trip == null) {
+      return emptyContainer;
+    }
+
+    final routeId = trip.route_id;
+    final route = routesLookup[routeId];
+
+    if (route == null) {
+      return emptyContainer;
+    }
+
+    final routeShortName = route.route_short_name ?? '';
+    final routeTextColor = route.parsedRouteTextColor;
+    final routeColor = route.parsedRouteColor;
+
+    final angle = radians(vehiclePosition.position.bearing) + pi * 3 / 4;
+
+    final rotatedColorContainer = Transform.rotate(
+      angle: angle,
+      alignment: Alignment.center,
+      child: Container(
+        //alignment: Alignment.center,
+        width: 25,
+        height: 25,
+        decoration: BoxDecoration(
+          color: routeColor,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(100.0),
+              bottomRight: Radius.circular(100.0),
+              topLeft: Radius.circular(100.0),
+              bottomLeft: Radius.circular(0.0)),
+        ),
+      ),
+    );
+
+    final componentStack = Stack(
+      children: <Widget>[
+        rotatedColorContainer,
+        Container(
+          child: Text(
+            routeShortName,
+            style: TextStyle(color: routeTextColor),
+          ),
+          alignment: Alignment.center,
+          padding: const EdgeInsets.fromLTRB(0, 0, 6, 6),
+        ),
+      ],
+    );
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          NavigatorRoutes.routeTrip,
+          arguments: TripScreenArguments(route: route, trip: trip, stop: null),
+        );
+      },
+      child: componentStack,
+    );
+  }
+
+  Widget getEmptyContainer(rt.VehiclePosition vehiclePosition) {
+    final angle = radians(vehiclePosition.position.bearing) + pi * 3 / 4;
+
+    final rotatedColorContainer = Transform.rotate(
+      angle: angle,
+      alignment: Alignment.center,
+      child: Container(
+        //alignment: Alignment.center,
+        width: 25,
+        height: 25,
+        decoration: BoxDecoration(
+          color: const Color(0xFF42A5F5),
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(100.0),
+              bottomRight: Radius.circular(100.0),
+              topLeft: Radius.circular(100.0),
+              bottomLeft: Radius.circular(0.0)),
+        ),
+        //padding: const EdgeInsets.fromLTRB(0, 0, 16, 16),
+      ),
+    );
+
+    final componentStack = Stack(
+      children: <Widget>[
+        rotatedColorContainer,
+        Container(
+          child: Icon(
+            Icons.directions_bus,
+            size: 15,
+          ),
+          alignment: Alignment.center,
+          padding: const EdgeInsets.fromLTRB(0, 0, 6, 5),
+        ),
+      ],
+    );
+
+    return componentStack;
   }
 }
